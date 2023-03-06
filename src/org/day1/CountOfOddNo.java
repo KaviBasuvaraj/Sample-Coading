8c12ff46b765e39e631cd1e9b7802dab87f502f4
KaviBasuvaraj <kavinano7@gmail.com>
2023-03-07 02:32:30 +0530

    Count Odd number Programm Updated

--- src/org/day1/CountOfOddNo.java
+++ src/org/day1/CountOfOddNo.java
@@ -3,8 +3,9 @@ package org.day1;
 public class CountOfOddNo {
 	public static void main(String[] args) {
 		int count =0;
-		for(int i=0; i<=100;i++) {
+		for(int i=1; i<=100;i++) {
 			if(i%2!=0) {
+				System.out.println(i);
 				count++;
 			}
 		}
